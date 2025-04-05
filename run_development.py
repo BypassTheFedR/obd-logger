8d948722e6e912717383121bc25dfa2b3688c48f
BypassTheFedR <94949817+BypassTheFedR@users.noreply.github.com>
2025-04-05 09:12:29 -0700

    Add run development as main entry point

---
+++ run_development.py
@@ -0,0 +1,23 @@
+# main script for development purposes without flask to test the backend frame work
+from app.obd_interface import start_connection_thread
+from app.logger import start_logging_thread
+import time
+
+def main():
+    print("Starting OBD connection thread...")
+    start_connection_thread()
+
+    print("Starting data logger...")
+    start_logging_thread
+
+    print("Systems is running. Press Ctrl+C to exit.")
+
+    try:
+        while True:
+            time.sleep(1) # keeps main thread alive without hogging CPU??
+
+    except KeyboardInterrupt:
+        print("\nShutting down.")
+
+if __name__ == "__main__":
+    main()
